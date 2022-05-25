9336cedf03b80ffdbc853bb2fb37855a5150f351
msanmaz <osanmaz4992@gmail.com>
2022-05-25 21:22:48 +0100

    post req

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -3,12 +3,39 @@ import prisma from 'lib/prisma'
 
 
 export default async function handler(req, res) {
-  if(req.method !== 'GET') {
-    res.status(405).json({
-      message:'Method Not Allowed'
-    })
+  if(req.method == 'GET') {
+    const trips = await prisma.Trip.findMany()
+    res.status(200).json(trips)
     return
   }
-  const trips = await prisma.Trip.findMany()
-  res.status(200).json(trips)
+
+  if(req.method === 'POST') {
+    const {user, name, start_date, end_date} = req.body
+
+    if(!user) {
+      return res
+      .status(400)
+      .json({message:'Missing required params `user` '})
+    }
+    if(!name) {
+      return res
+      .status(400)
+      .json({message:'Missing required params `name` '})
+    }
+
+    await prisma.Trip.create({
+      data:{
+        user,
+        name,
+        start_date,
+        end_date,
+      }
+    })
+
+    return res.status(200).end()
+ 
+  }
+
+  res.status(405).json({message:'Method Not Allowed'})
+
 }
